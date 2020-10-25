f74fcf32641bfc22057724b583e0fcfeffba3226
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-10-25 21:51:07 +0530

    feat: SUBMISSIONS - 5.2 5.3 5.4 - add functionality to add new blog and notify users of operations via toast notifications

--- bloglist-frontend/src/components/BlogListing.js
+++ bloglist-frontend/src/components/BlogListing.js
@@ -6,6 +6,83 @@ import CONSTANTS from "../lib/constants.js";
 
 import blogService from "../services/blogs.js";
 
+import { useToasts } from "react-toast-notifications";
+
+const BlogEntryForm = ({ refreshBlogList, user }) => {
+
+    const [title,setTitle] = useState("");
+    const [url,setUrl] = useState("");
+
+    const { addToast } = useToasts();
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value);
+    }
+
+    const handleAddBlogSubmit = (event) => {
+        (async() => {
+            event.preventDefault();
+            try{
+                
+                if(title.length===0 || url.length===0){
+                    console.log("handleAddBlogSubmit err condition");
+                    throw new Error("Fields cannot be empty");
+                }
+
+                const addBlogResult = await blogService.addNewBlog({
+                    title: title,
+                    url: url
+                },user.token);
+
+                addToast(`Blog "${title}" added`,{
+                    appearance: "success",
+                    autoDismiss: true,
+                });
+
+                refreshBlogList();
+
+            }catch(e){
+                console.error(`BlogEntryForm|ERROR`,e.message);
+                addToast(e.message || "AN ERROR OCCURRED",{
+                    appearance: "error",
+                    autoDismiss: true,
+                });
+            }
+        })();
+    }
+
+
+    return (<div>
+        <form onSubmit={handleAddBlogSubmit}>
+        <div>
+          title
+            <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          url
+            <input
+            type="text"
+            value={url}
+            name="URL"
+            onChange={handleUrlChange}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+        </div>)
+
+
+}
+
 const BlogListing = ({user,setUser}) => {
 
     const [blogs,setBlogs] = useState([]);
@@ -28,6 +105,7 @@ const BlogListing = ({user,setUser}) => {
 
     return (<div>
         <p>Hello, {user.username} <button onClick={logout}>logout</button> </p>
+        <BlogEntryForm refreshBlogList={refreshBlogList} user={user} />
         <h2>blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -6,4 +6,36 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const addNewBlog = (blogDetails,token) => {
+  return new Promise((resolve,reject) => {
+    (async() => {
+      try{
+        const blogAdditionResult = await axios({
+            method: "POST",
+            url: `http://localhost:3001/api/blogs`,
+            headers: {
+              "Authorization": `Bearer ${token}`
+            },
+            data: blogDetails
+          });
+
+          resolve(blogAdditionResult.data);
+
+      }catch(e){
+        if(e.response){
+          reject(e.response.data);
+        } else if(e.request){
+          reject({
+            message: "NO RESPONSE FROM SERVER"
+          })
+        } else {
+          reject({
+            method: "AN ERROR OCCURRED"
+          })
+        }
+      }
+    })();
+  })
+}
+
+export default { getAll, addNewBlog }
