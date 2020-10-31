d8042fe7edd3d6b8caeb3b574d93152cd8bfa021
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-10-31 18:33:41 +0530

    feat: SUBMISSION - 5.7 - add button to toggle for full details of blog in Blog component

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/Blog.css'
 
-const Blog = ({ blog }) => (
-  <div className="blog-container">
-    {blog.title}<br />
-    by {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [isDetailsVisible,setIsDetailsVisible] = useState(false)
+
+
+  const detailsClassNames = isDetailsVisible ? '' : 'hidden'
+
+  const toggleBlogDetailsVisibility = () => {
+    setIsDetailsVisible(!isDetailsVisible)
+  }
+
+  return (
+    <div className="blog-container">
+      <div className="blog-title">
+        {blog.title}<br />
+        by {blog.author}
+      </div>
+      <div className={detailsClassNames}>
+        <hr />
+        Blog URL: {blog.url}<br />
+        Likes: {blog.likes} &nbsp;
+        <button>
+          Like
+        </button>
+      </div>
+      <button onClick={toggleBlogDetailsVisibility}>
+        {isDetailsVisible ? 'Close Details' : 'View Details'}
+      </button>
+    </div>
+  )
+
+}
 
 export default Blog
