a87dffaa2e35ef834ce62ea184a6c1dd8f9f7e04
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-11-01 21:16:12 +0530

    refactor: separate LikesContainer into its own component

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,39 +1,13 @@
 import React, { useState } from 'react'
 import { useToasts } from 'react-toast-notifications'
 
+import LikesContainer from './LikesContainer.js'
+
 import blogService from '../services/blogs.js'
 
 import './css/Blog.css'
 
 
-const LikesContainer = ({ likesCount, blogId }) => {
-  const [likes,setLikes] = useState(likesCount || 0)
-
-  const { addToast } = useToasts()
-
-  const addLike = () => {
-    (async() => {
-      try{
-        const likeAdditionResult = await blogService.addLikeToBlog(blogId)
-
-        setLikes(likes+1)
-
-      }catch(e){
-        addToast(e.message || 'AN UNKNOWN ERROR OCCURRED',{
-          appearance: 'error',
-          autoDismiss: true
-        })
-      }
-    })()
-  }
-
-  return (<>
-    Likes: {likes} &nbsp;
-    <button onClick={addLike}>
-          Like
-    </button>
-  </>)
-}
 
 
 const Blog = ({ blog, refreshBlogList, user }) => {
