bdab49b6fac9e3eb06ccb2519dc10d18cf8546bc
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-11-08 17:27:26 +0530

    test: add cypress test to check for error in login if nothing entered

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,3 +1,6 @@
+
+const BASE_URL = 'http://localhost:3000'
+
 describe('Blog app', function() {
 
   beforeEach(() => {
@@ -6,9 +9,23 @@ describe('Blog app', function() {
 
 
   it('front page shows login', function() {
-    cy.visit('http://localhost:3000')
+    cy.visit(BASE_URL)
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
   })
+
+
+  describe('Login', function(){
+
+    it('should give malformed auth header with nothing entered',function(){
+      cy.visit(BASE_URL)
+
+      cy.contains('login').click()
+
+      cy.contains('MALFORMED AUTH HEADER')
+    })
+
+  })
+
 })
