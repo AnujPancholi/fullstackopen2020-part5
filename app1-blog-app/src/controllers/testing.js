c8ad6f22faca7af1e8b09388f19e93d73a3f05e2
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-11-14 15:13:37 +0530

    feat: add blogset for testing in reset endpoint, testing route

--- app1-blog-app/src/controllers/testing.js
+++ app1-blog-app/src/controllers/testing.js
@@ -4,6 +4,8 @@ const testingRouter = require('express').Router()
 
 const BlogModel = require('../models/blogs.js');
 const UserModel = require('../models/users.js');
+
+const mongooseUtils = require('../utils/mongooseUtils.js');
 const logger = require('../utils/logger.js');
 const bcrypt = require('bcrypt');
 // const mongooseUtils = require("../utils/mongooseUtils.js");
@@ -11,6 +13,27 @@ const bcrypt = require('bcrypt');
 testingRouter.get('/ping',(req,res,next) => {
   res.status(200).send();
 })
+
+const TEST_BLOG_SETS = {
+  "DEFAULT": [],
+  "SET1": [{
+    "title": "Mock Blog Title",
+    "author": "Random Author",
+    "url": "https://www.mockblog.com/123456",
+    "likes": 4
+  },{
+    "title" : "Another blog",
+    "url" : "http://adflkj.sdlfkja.lkjdf",
+    "likes" : 2,
+    "author" : "Bloggy McBloggison",
+  },{
+    "title" : "THE NATION WANT TO KNOW",
+    "url" : "http://arnub.barnub.com",
+    "likes" : 3,
+    "author" : "Blognab GoBlamy"
+  }]
+}
+
 
 testingRouter.post('/reset',async(req,res,next) => {
 
@@ -34,7 +57,18 @@ testingRouter.post('/reset',async(req,res,next) => {
 
     const testUser = new UserModel(testUserObj)
 
-    await testUser.save()
+    const testUserSaveResult = await testUser.save()
+
+    const testBlogSet = TEST_BLOG_SETS[req.query.blogset || "DEFAULT"]
+
+    if(testBlogSet.length>0){
+      testBlogSet.forEach(testBlog => {
+        testBlog.userId = mongooseUtils.getObjectId(testUserSaveResult._id)
+      })
+      await BlogModel.insertMany(testBlogSet);
+    }
+
+    
     
     res.status(204).send();
   } catch(e) {
